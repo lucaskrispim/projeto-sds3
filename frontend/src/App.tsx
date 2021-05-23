3b5252dd4d7b5c65f509d34e5462d58f765491cb
Lucas wilman da silva crispim <lucas.crispim@intelie.com.br>
2021-05-23 18:33:39 -0300

    basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from "components/datatable";
+import Footer from "components/footer";
+import Navbar from "components/navbar";
+import React from "react";
+
 function App() {
   return (
-    <div >
+    <>
+    <Navbar/>
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
