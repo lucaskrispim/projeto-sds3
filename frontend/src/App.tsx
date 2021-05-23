91643a1760de82d7bdf27c3db3e4aec57d0136c5
Lucas wilman da silva crispim <lucas.crispim@intelie.com.br>
2021-05-23 20:16:12 -0300

    static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,29 @@
 import DataTable from "components/datatable";
 import Footer from "components/footer";
 import Navbar from "components/navbar";
+import BarChart from "components/barchart";
 import React from "react";
+import DonutChart from "components/donutchart";
 
 function App() {
   return (
     <>
     <Navbar/>
     <div className="container">
-      <h1 className="text-primary">Olá mundo!</h1>
+      <h1 className="text-primary py-3">Dashboard de vendas</h1>
+      <div className="row px-3">
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
+          <BarChart/>
+        </div>
+        <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Todas as vendas</h5>
+          <DonutChart/>
+        </div>
+      </div>
+      <div className="py-3">
+        <h2 className="text-primay">Todas vendas</h2>
+      </div>
       <DataTable/>
     </div>
     <Footer/>
