7399b816b87dac63ade2ec14254c980775456b4b
Lucas wilman da silva crispim <lucas.crispim@intelie.com.br>
2021-05-27 13:53:12 -0300

    routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,8 @@
-import DataTable from "components/datatable";
-import Footer from "components/footer";
-import Navbar from "components/navbar";
-import BarChart from "components/barchart";
-import React from "react";
-import DonutChart from "components/donutchart";
+import Routes from "Routes";
 
 function App() {
   return (
-    <>
-    <Navbar/>
-    <div className="container">
-      <h1 className="text-primary py-3">Dashboard de vendas</h1>
-      <div className="row px-3">
-        <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Taxa de sucesso (%)</h5>
-          <BarChart/>
-        </div>
-        <div className="col-sm-6">
-          <h5 className="text-center text-secondary">Todas as vendas</h5>
-          <DonutChart/>
-        </div>
-      </div>
-      <div className="py-3">
-        <h2 className="text-primay">Todas vendas</h2>
-      </div>
-      <DataTable/>
-    </div>
-    <Footer/>
-    </>
+    <Routes/>
   );
 }
 
