52bd02678e6fa49ec096ec2f72afca3396eff2a2
Lucas wilman da silva crispim <lucas.crispim@intelie.com.br>
2021-05-27 18:55:57 -0300

    home, navigation

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,9 +1,22 @@
-
+import React from "react"
+import { Link } from "react-router-dom"
+import Navbar from "../../components/navbar";
+import Footer from "../../components/footer";
 const Home = () => {
   return (
-    <div>
-      <h1>Página home</h1>
+    <>
+    <Navbar/>
+    <div className="container">
+      <div className="jumbotron">
+        <h1 className="display-4">DSVendas</h1>
+        <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+        <hr />
+        <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+        <Link className="btn btn-primary btn-lg" to="/dashboard">Acessar o dashboard</Link>
+      </div>
     </div>
+    <Footer/>
+    </>
   );
 }
 
